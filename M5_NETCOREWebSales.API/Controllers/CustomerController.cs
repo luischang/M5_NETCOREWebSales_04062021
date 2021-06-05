9bc5c3638e77ef06156f13306d23e2ebd6a80048
Luis Chang <luis.chang@outlook.com>
2021-06-04 23:39:47 -0500

    CRUD Customer API

--- M5_NETCOREWebSales.API/Controllers/CustomerController.cs
+++ M5_NETCOREWebSales.API/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using M5_NETCOREWEBSales.Core.Interfaces;
+﻿using M5_NETCOREWEBSales.Core.Entities;
+using M5_NETCOREWEBSales.Core.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -21,12 +22,48 @@ namespace M5_NETCOREWebSales.API.Controllers
 
 
         [HttpGet]
+        [Route("GetCustomer")]
         public async Task<IActionResult> GetCustomer()
         {
             var customers = await _customerRepository.GetCustomers();
             return Ok(customers);
         }
 
+        [HttpGet]
+        [Route("CustomerById/{id}")]
+        public async Task<IActionResult> CustomerById(int id)
+        {
+            var customer = await _customerRepository.GetCustomerById(id);
+            if (customer == null)
+                return NotFound();
+            return Ok(customer);
+        }
 
+        [HttpPost]
+        [Route("PostCustomer")]
+        public async Task<IActionResult> PostCustomer(Customer customer)
+        {
+            await _customerRepository.InsertCustomer(customer);
+            return Ok(customer);
+        
+        }
+
+        [HttpPut]
+        [Route("PutCustomer")]
+        public async Task<IActionResult> PutCustomer(Customer customer)
+        {
+            await _customerRepository.UpdateCustomer(customer);
+            return Ok(customer);
+        }
+
+        [HttpDelete]
+        [Route("DeleteCustomer/{id}")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            var result = await _customerRepository.DeleteCustomer(id);
+            if (!result)
+                return BadRequest();
+            return NoContent();
+        }
     }
 }
